3f41d052a2cc82bb1ae2609625ad1546a6b022fe
Harrylever <ukanah15thdean@gmail.com>
2022-06-21 11:59:38 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let html_Text = '';
+html_Text += "Name: Ukanah Dean <br>";
+html_Text += "Height: 6.2 feet <br>"
+html_Text += "Country: Nigeria"
+
+document.getElementById("text").innerHTML = html_Text
